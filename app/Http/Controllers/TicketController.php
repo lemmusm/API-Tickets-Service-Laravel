5ca8bddb86e402859723731cdd4a688dd32ba101
Manuel Hernandez Lemus <lemmusm@gmail.com>
2019-01-09 22:37:59 -0600

    api tickets

--- app/Http/Controllers/TicketController.php
+++ app/Http/Controllers/TicketController.php
@@ -14,17 +14,10 @@ class TicketController extends Controller
      */
     public function index()
     {
-        //
-    }
-
-    /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function create()
-    {
-        //
+        return Ticket::with('usuario')
+        // ->join('usuarios', 'usuario_uid', '=', 'usuarios.uid' )
+        // ->join('departamentos', 'departamento_id', '=', 'departamentos.id_departamento' )
+        ->get();
     }
 
     /**
@@ -35,7 +28,31 @@ class TicketController extends Controller
      */
     public function store(Request $request)
     {
-        //
+        $ticket = new Ticket();
+
+        if (!is_null($ticket)) {
+
+            $ticket -> usuario_uid = $request -> usuario_uid;
+            $ticket -> servicio = $request -> servicio;
+            $ticket -> descripcion = $request -> descripcion;
+            $ticket -> diagnostico = $request -> diagnostico;
+            $ticket -> tecnico = $request -> tecnico;
+            $ticket -> status = $request -> status;
+            $ticket -> save();
+
+            $response = array(
+                'status' => 'success',
+                'code' => 200,
+                'message' => 'Ticket creado correctamente.'
+            );
+        } else {
+            $response = array(
+                'status' => 'error',
+                'code' => 400,
+                'message' => 'Error al crear ticket.'
+            );
+        }
+        return $response;
     }
 
     /**
@@ -44,20 +61,22 @@ class TicketController extends Controller
      * @param  \App\Models\Ticket  $ticket
      * @return \Illuminate\Http\Response
      */
-    public function show(Ticket $ticket)
+    public function show($id)
     {
-        //
-    }
+       $ticket = Ticket::with('usuario')->find($id);
 
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  \App\Models\Ticket  $ticket
-     * @return \Illuminate\Http\Response
-     */
-    public function edit(Ticket $ticket)
-    {
-        //
+       if ($ticket) {
+           return $ticket;
+       } else {
+           $response = array(
+               'status' => 'error',
+               'code' => 400,
+               'message' => 'Registro no encontrado.'
+           );
+       }
+
+       return $response;
+       
     }
 
     /**
@@ -67,9 +86,27 @@ class TicketController extends Controller
      * @param  \App\Models\Ticket  $ticket
      * @return \Illuminate\Http\Response
      */
-    public function update(Request $request, Ticket $ticket)
+    public function update(Request $request, $id)
     {
-        //
+        $ticket = new Ticket();
+
+        $ticket = Ticket::where('id_ticket', $id)->update(
+            [
+                'servicio' => $request -> get('servicio'),
+                'descripcion' => $request -> get('descripcion'),
+                'diagnostico' => $request -> get('diagnostico'),
+                'tecnico' => $request -> get('tecnico'),
+                'status' => $request -> get('status')
+            ]
+        );
+
+        $response = array(
+            'status' => 'success',
+            'code' => 200,
+            'message' => 'Ticket actualizado correctamente.'
+        );
+
+        return $response;
     }
 
     /**
@@ -78,8 +115,24 @@ class TicketController extends Controller
      * @param  \App\Models\Ticket  $ticket
      * @return \Illuminate\Http\Response
      */
-    public function destroy(Ticket $ticket)
+    public function destroy($id)
     {
-        //
+       if ($id != null) {
+           $ticket = Ticket::find($id);
+           $ticket->delete();
+
+           $response = array(
+               'status' => 'success',
+               'code' => 200,
+                'message' => 'Ticket eliminado correctamente.'
+           );
+       } else {
+           $response = array(
+               'status' => 'error',
+               'code' => 400,
+                'message' => 'Error al eliminar ticket.'
+           );
+       }
+       return $response;
     }
 }
--- app/Models/Ticket.php
+++ app/Models/Ticket.php
@@ -3,8 +3,19 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use App\Models\Usuario;
 
 class Ticket extends Model
 {
-    //
+    // Nombre de la tabla en MySQL.
+	protected $table='tickets';
+
+	// Eloquent asume que cada tabla tiene una clave primaria con una columna llamada id.
+	// Si éste no fuera el caso entonces hay que indicar cuál es nuestra clave primaria en la tabla:
+	protected $primaryKey = 'id_ticket';
+	
+    public function usuario()
+    {
+        return $this->belongsTo(Usuario::class, 'usuario_uid', 'uid');
+    }
 }
